24dba59b5105e3b2f6a5d5571acdedc27665918e
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 14:20:15 +0800

    [fizzBuzz][HinsChiu]feat:implement case that order given multiple of seven

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,17 +3,22 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     private static final String countOffForMultipleOfThree = "Fizz";
     private static final String countOffForMultipleOfFive = "Buzz";
+    private static final String countOffForMultipleOfSeven = "Whizz";
 
     public String countingOff(int order) {
         String countOff = String.valueOf(order);
         boolean isMultipleOfThree = this.isMultipleOfThree(order);
         boolean isMultipleOfFive = this.isMultipleOfFive(order);
+        boolean isMultipleOfSeven = this.isMultipleOfSeven(order);
 
         if(isMultipleOfThree) {
             countOff = countOffForMultipleOfThree;
         }
         else if(isMultipleOfFive) {
             countOff = countOffForMultipleOfFive;
+        }
+        else if(isMultipleOfSeven) {
+            countOff = countOffForMultipleOfSeven;
         }
 
         return countOff;
@@ -25,5 +30,9 @@ public class FizzBuzz {
 
     private boolean isMultipleOfFive(int order) {
         return order % 5 == 0;
+    }
+
+    private boolean isMultipleOfSeven(int order) {
+        return order % 7 == 0;
     }
 }
