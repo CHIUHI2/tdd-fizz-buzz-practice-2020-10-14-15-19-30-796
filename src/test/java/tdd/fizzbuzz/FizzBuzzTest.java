2a7e21a1a29a39c0d62b8da106ed7a8d4ae8af08
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 14:10:59 +0800

    [fizzBuzz][HinsChiu]test:implement test for case order given multiple of three

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private final String countOffForMultipleOfThree = "Fizz";
+
     @Test
     void should_return_order_number_when_order_given_not_special_number() {
         //given
@@ -14,5 +16,16 @@ public class FizzBuzzTest {
         String countOff = fizzBuzz.countingOff(order);
         //then
         assertEquals(String.valueOf(order), countOff);
+    }
+
+    @Test
+    void should_return_fizz_when_order_given_multiple_of_three() {
+        //given
+        int order = 3;
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String countOff = fizzBuzz.countingOff(order);
+        //then
+        assertEquals(this.countOffForMultipleOfThree, countOff);
     }
 }
