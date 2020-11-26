c96eba4cb3761aa1c5b40c1d63a14deeda8e6fe2
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 14:13:46 +0800

    [fizzBuzz][HinsChiu]feat:implement case that order given multiple of three

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,20 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private final String countOffForMultipleOfThree = "Fizz";
+
     public String countingOff(int order) {
         String countOff = String.valueOf(order);
+        boolean isMultipleOfThree = this.isMultipleOfThree(order);
+
+        if(isMultipleOfThree) {
+            countOff = this.countOffForMultipleOfThree;
+        }
 
         return countOff;
+    }
+
+    private boolean isMultipleOfThree(int order) {
+        return order % 3 == 0;
     }
 }
