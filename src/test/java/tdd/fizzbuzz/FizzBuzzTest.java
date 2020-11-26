5ebef2a3790968318afced88a1e13d7403286af6
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 14:06:12 +0800

    [fizzBuzz][HinsChiu]test:implement test for normal case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_number_when_order_given_not_special_number() {
+        //given
+        int order = 1;
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String countOff = fizzBuzz.countingOff(order);
+        //then
+        assertEquals(String.valueOf(order), countOff);
+    }
 }
